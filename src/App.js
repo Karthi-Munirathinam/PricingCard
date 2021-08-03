e43e2b593bdfaca28fc3d3678f72343e13b9c135
Karthi <karthiraja268@gmail.com>
2021-08-03 16:50:50 +0530

    PriceCard V 0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import PriceCard from './priceCard';
 import './App.css';
 
 function App() {
+  const pricingData = [
+    {
+      packageName: "FREE",
+      price: "$0",
+      available: ["Single User", "5GB Storage", "Unlimited Public Projects", "Community Access"],
+      unavailable: ["Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain", "Monthly Status Reports"]
+    },
+    {
+      packageName: "PLUS",
+      price: "$9",
+      available: ["5 Users", "50GB Storage", "Unlimited Public Projects", "Community Access", "Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain"],
+      unavailable: ["Monthly Status Reports"]
+    },
+    {
+      packageName: "PRO",
+      price: "$49",
+      available: ["Unlimited Users", "150GB Storage", "Unlimited Public Projects", "Community Access", "Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain", "Monthly Status Reports"]
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+
+          {pricingData.map((obj, index) => {
+            return <PriceCard key={`C${index}`} Data={obj} />
+          })}
+
+        </div>
+      </div>
+    </section>
   );
 }
 
